b857633a5085cd20ec65155ef8b379a8e7cd607e
Avila-Pato <p.avilaf1998@gmail.com>
2025-04-28 21:47:24 -0400

    Add drawPoint.py to implement a pattern printing functionality

---
+++ Juegos/drawPoint.py
@@ -0,0 +1,7 @@
+
+
+for i in range(10):
+    for j in range(i):
+        print(" * " , end="")
+    print()
+ ##Para cada i, for j in range(i): → se repite i veces, imprimiendo " * " en la misma línea (end="" evita el salto de línea).
