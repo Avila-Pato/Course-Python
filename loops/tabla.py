2c4699161fd3c4e2fccf9b833418361f205cb5fd
Avila-Pato <p.avilaf1998@gmail.com>
2025-05-06 13:45:26 -0400

    Fix enumerate usage and improve input handling in multiplication and parity check programs

--- loops/tabla.py
+++ loops/tabla.py
@@ -13,3 +13,16 @@ user = int(input("Ingrese un numero \n"))
 
 for i in range(1, user):
     print(f"{user} X {i } = {user * (i) }")
+
+nombre = ['Pato', 'Luis', 'Felipe', 'Catalina']
+
+for i in enumerate[nombre]:
+    print(nombre)
+
+date = int(input("ingreso un numero"))
+
+for i in range(1,  date):
+    if i % 2 == 0:
+        print(f"es par {i}")
+    else:
+        print(f"es impar {i} ")
