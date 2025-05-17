f3361798b4407428006ff785d3967f22005d6720
Avila-Pato <p.avilaf1998@gmail.com>
2025-05-17 00:39:28 -0400

    Refactor word guessing game to improve user prompts and code clarity

--- Juegos/adivinaPalabra.py
+++ Juegos/adivinaPalabra.py
@@ -1,4 +1,9 @@
 import random
+import os
+
+# Limpiar pantalla
+if os.system("clear") != 0:
+    os.system("cls")
 
 # Lista de palabras
 cadena = ["cerdo", "loro", "ballena", "langosta"]
@@ -14,17 +19,21 @@ print("Palabra a adivinar:", " ".join(progreso))
 
 # Simular intentos del usuario
 while "_" in progreso:
-    letra = input("Ingresa una letra: ").lower()
+    letra = input("Ingresa una letra o intenta adivinar toda la palabra: ").lower()
 
-    if letra in palabra:
-        # Reemplazar "_" con la letra en posiciones correctas
+    if letra == palabra:
+        print("¡Lo lograste! La palabra era:", palabra)
+        progreso = list(palabra)  # Mostrar la palabra completa
+        break
+    elif letra in palabra:
         for i, l in enumerate(palabra):
             if l == letra:
                 progreso[i] = letra
         print("¡Bien! Progreso:", " ".join(progreso))
     else:
-        print("Letra incorrecta. Intenta otra vez.")
-    
-    if "_" not in progreso:
-        print("¡Felicidades! Adivinaste la palabra:", palabra)
-        break
+        print("Inténtalo de nuevo.")
+
+# Verificar si se adivinó por letras
+#si ya no hay mas "_" en la letra que esta adivinando significa que el usuario adivino toda la palabra
+if "_" not in progreso:
+    print("¡Felicidades! Adivinaste la palabra:", palabra)
