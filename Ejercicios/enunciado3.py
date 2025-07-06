53461271ddbf31b695269a929fe397982e68ae0f
Patricio Avila <152195893+Avila-Pato@users.noreply.github.com>
2025-07-06 17:18:03 -0400

    Add student registration and grading system: implement functionalities to add, view, and manage student records with error handling and interactive menu.

---
+++ Ejercicios/enunciado3.py
@@ -0,0 +1,99 @@
+
+# Enunciado 3: Registro de Estudiantes y Notas 
+
+# Descripción: Este sistema permite registrar estudiantes y sus notas en diferentes asignaturas. Cada estudiante tendrá un nombre, una matrícula única y una lista de notas. 
+
+# Características Clave: 
+
+# •	Uso de un diccionario donde las claves son las matrículas de los estudiantes y los valores son diccionarios con el nombre y una lista de notas. 
+
+# •	Funciones para añadir estudiantes, añadir notas, consultar notas, calcular promedios y eliminar estudiantes. 
+
+# •	Control de errores para matrículas duplicadas, notas inválidas y estudiantes no encontrados. 
+
+# •	Ciclos para el menú y para procesar las notas. 
+
+ 
+
+estudiantes = {
+    '2025001': {
+        'nombre': 'Ana Martínez',
+        'asignaturas': {
+            'Matemáticas': [6.5, 5.8, 6.9],
+            'Biología': [5.5, 6.2],
+            'Lenguaje': [3.3, 1.3],
+            'Programacion':[6.4, 7.0]
+        }
+    },
+}
+ 
+def Menu():
+    print("1- Agregar Estudiante")
+    print("2- Consultar Estudiante")
+    print("3- Calcular Promedio")
+    print("4- Eliminar Estudiante")
+    print("5- Salir")
+    
+def Agregar_estudiante():
+    # matricula = input("Ingrese al amtricula del estudiante: ")
+    # nombre = input("Ingrese el nombre dele estudiante: ")
+    # notas = list(map(float, input("Ingrese las notas separadas por coma").split(",")))
+    
+    # estudiantes[matricula] = {
+    #     'nombre': nombre,
+    #     'notas': notas
+    # }
+    
+    
+    matricula = input("Ingrese la matrícula del estudiante: ")
+    
+    if matricula in estudiantes:
+        print("Ya existe ese estudiante cone s matricula")
+        return
+    nombre = input("Ingrese el nombre del estudiante: ")
+    asignaturas = {}
+    
+    while True:
+        asignatura = input("Nombre de la asignatura o escriba 'fin' para terminare: ")
+        if asignatura.lower() == 'fin':
+            break
+        
+        try:
+            notas = list(map(float, input("Ingrese las notas separadas por coma: ").split(",")))
+            asignaturas[asignatura] = notas ##se cre aun nuevo diccionario que alamcena las notas de las asignaturas estas se pasan al diccionario estudaintes 
+        except:
+            print("Ingrese solo numeros validos")
+            
+    estudiantes[matricula] = {
+         'nombre': nombre,
+         'asignaturas': asignaturas
+     }
+
+def Consultar_estudiante():
+    print(estudiantes)
+def Calcular_promedio():
+    pass
+def Eliminar_estudiante():
+    pass
+
+def Salir():
+    print("Saliendo..")
+
+while True:
+    Menu()
+    opc = input("Ingrese una opcion: ")
+
+    match opc:
+        case "1":
+            Agregar_estudiante()
+        case "2":
+            Consultar_estudiante()
+        case "3":
+            Calcular_promedio()
+        case "4":
+            Eliminar_estudiante()
+        case "5":
+            Salir()
+            break
+        case _:
+            print("Ingrese una opcion correcta ")
---
+++ Ejercicios/enunciado4.py
@@ -0,0 +1,20 @@
+
+# Enunciado 4 : Gestión de Biblioteca 
+
+# Descripción: Este sistema permite gestionar los libros de una biblioteca. Se podrán añadir, consultar, modificar y eliminar libros. Cada libro tendrá un ID único, un título, un autor, un año de publicación y un estado (disponible/prestado). 
+
+# Características Clave: 
+
+# •	Uso de un diccionario para almacenar los libros, donde la clave es el ID del libro. 
+
+# •	Menú interactivo para navegar por las opciones. 
+
+# •	Funciones separadas para cada operación: añadir, consultar, modificar y eliminar. 
+
+# •	Control de errores robusto para entradas inválidas, libros no encontrados y IDs duplicados. 
+
+# •	Ciclos while para el menú principal y for para iterar sobre los libros. 
+
+# •	Decisiones if/elif/else para la lógica del menú y las operaciones. 
+
+# # •	No se utiliza class, todo se maneja con funciones y estructuras de datos básicas. 
